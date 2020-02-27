0ad5c235f7ba1c29816dfed47007781a8b3800a4
Kensen <hirohtk@gmail.com>
2020-02-26 20:37:57 -0800

    nodemailer setup

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,17 +1,20 @@
 const router = require("express").Router();
 const db = require("../model/comments");
+const nodemailer = require("nodemailer");
+
+require('dotenv').config()
 
 let emailer = (recipient, subject, message) => {
     var transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'volunteamsters@gmail.com',
-            pass: ''
+            user: 'kensen.hirohata@gmail.com',
+            pass: process.env.gmailPassword
         }
     });
 
     var mailOptions = {
-        from: 'volunteamsters@gmail.com',
+        from: 'kensen.hirohata@gmail.com',
         to: recipient,
         subject: subject,
         text: message
@@ -31,28 +34,26 @@ router.post("/api/message", function (req, res) {
     console.log(req.body)
 
     db.create(req.body)
-    .then(function (response) {
-        let emailBot = (input) => {
+        .then(function (response) {
 
-            let emailList = [];
-            for (i = 0; i < input.length; i++) {
-                db.Users.findById(input.attendees[i]).select("email").then( (response ) => {
-                emailList.push(response);
-                }) 
-            }
-            emailer(emailList.toString, "Test!",
-                    `Your event has been changed!  Here are the details \n
-                    `, () => {
-                });
-        }
-        emailBot(response);
-        
+                let emailList = `${req.body.email}, hirohtk@gmail.com`;
+
+                emailer(
+                    emailList,
+                    "Kensen's Portfolio Messager",
+                    `Hello ${req.body.name}, \n
+                    I appreciate you getting in touch with me through my portfolio.  Please lookout for a response from my email address hirohtk@gmail.com.\n
+                    Best regards,\n
+                    Kensen Hirohata
+                    `
+                );
+
             res.json(response)
         })
-    .catch(function (err) {
-        console.log(err);
-        res.status(422).json(err)
-    })
+        .catch(function (err) {
+            console.log(err);
+            res.status(422).json(err)
+        })
 
 });
 
